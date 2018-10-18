e1c0ced021f7846520e4ae8d41473e5bb029a8da
ktochow1 <ktochow1@binghamton.edu>
2018-10-18 18:38:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,10 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index  
+  array.each_with_index.collect do |element, index|
+    if index != 1 
+      element + "s"
+    end
 end
 
 
