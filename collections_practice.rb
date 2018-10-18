5c4e6b6e2fdd14a0e2353d91b497cd06217a9e64
ktochow1 <ktochow1@binghamton.edu>
2018-10-18 18:34:58 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,7 +23,7 @@ def swap_elements(array)
 end
 
 
-def reverse_array(array) #[12, 4, 35]
+def reverse_array(array) 
   array.reverse
 end
 
@@ -52,11 +52,6 @@ def add_s(array)
   
 end
 
-# a=[11,22,31,224,44].each_with_index do |val,index| puts "index: #{index} for #{val}" if val < 30
-# end
-#   index: 0 for 11
-#   index: 1 for 22
-#   => [11, 22, 31, 224, 44]
 
 
 
