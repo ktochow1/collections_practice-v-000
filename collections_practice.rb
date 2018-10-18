14501a05957e956357e7106463826d61351116d2
ktochow1 <ktochow1@binghamton.edu>
2018-10-18 18:41:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,9 +50,10 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    if index != 1 
+    if index != 1  
       element + "s"
-    end
+    else 
+      element
 end
 end
 
